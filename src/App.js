12e04907b67687c965a509b4e730bb3bb5eaf1d2
ASV185 <74805696+ASV185@users.noreply.github.com>
2021-09-18 23:01:51 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import image from "./blue.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={image} className="logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
