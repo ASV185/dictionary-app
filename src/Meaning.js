5583eb1cff96de0de2e209861b06476d39f333ba
ASV185 <74805696+ASV185@users.noreply.github.com>
2021-09-26 15:42:24 -0600

    added css to sections

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,21 +1,21 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props){
     return (
         <div className="Meaning">
-            <h3>{props.meaning.partOfSpeech}</h3>
+            <h4>{props.meaning.partOfSpeech}</h4>
             {props.meaning.definitions.map(function(definition,index) {
                 return(
                     <div key={index}>
-                        <p>
-                         <strong>Definitions:</strong>
-                           {definition.definition}
-                           <br/>
-                           <strong>Example:</strong>
-                           <em>{definition.example}</em>
-                           <Synonyms synonyms={definition.synonyms}/>
-                        </p>
+                        <div className="definition">
+                          Definition:{""} {definition.definition}
+                        </div>
+                        <div className="example">
+                           Example:{""} {definition.example}.
+                        </div>
+                         <span className="Synonyms">Synonyms: <Synonyms synonyms={definition.synonyms}/> </span>
                     </div>
                 );
             })}
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Synonyms.css";
 
 export default function Synonyms(props){
     if (props.synonyms){
         return (
             <ul className="Synonyms">
                 {props.synonyms.map(function(synonyms,index){
-                    return <li Key={index}>{synonyms}</li>;
+                    return <li Key={index}>/{synonyms}/</li>;
                 })}
             </ul>
         );
