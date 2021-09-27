8e410f7e842cde2b7402f8da539dd902e9638cc1
ASV185 <74805696+ASV185@users.noreply.github.com>
2021-09-26 21:13:18 -0600

    made minor corrections and added loader

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,38 +3,57 @@ import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
 import "./App";
+import Loader from "react-js-loader";
 
 export default function Dictionary(props){
 
-    let [keyword, setKeyword]=useState(props.defaultkeyword);
+    let [keyword, setKeyword]=useState(props.defaultKeyword);
     let [results, setResults]=useState(null);
+    let [loaded, setLoaded]=useState(false);
 
     function handleResponse(response){
         console.log(response.data[0]);
         setResults(response.data[0]);
     }
 
-    function search(event){
-        event.preventDefault();
+    function search(){
+     //documentation:https://dictionaryapi.dev/
         let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        console.log(apiUrl);
         axios.get(apiUrl).then(handleResponse);
     }
-       //documentation:https://dictionaryapi.dev/
+    function handleSubmit(event){
+        event.preventDefault();
+        search();
+    }
     function handlekeywordChange(event){
         setKeyword(event.target.value);
     }
-
+    function load(){
+        setLoaded(true);
+        search();
+    }
+    if (loaded){
     return(
         <div className="Dictionary">
             <h1>Ling-con</h1>
             <h2>/Dictionary App/</h2>
-            <form onSubmit={search}>
-                <input type="search" onChange={handlekeywordChange} defaultValue={props.defaultkeyword}/>
+            <form onSubmit={handleSubmit}>
+                <input type="search" onChange={handlekeywordChange} defaultValue={props.defaultKeyword}/>
                 <button type="search" className="button">Search</button>
             </form>
         
             <Results results={results}/>
         </div>
     );
+}else {
+    load();
+    return(
+        <div className={"row"}>
+            <div className={"item"}>
+                <Loader type="bubble-loop" bgColor={"#FFFFFF"}  color={'#0a2225'} size={100} />
+            </div>
+        </div>
+    );
+
+}
 }
