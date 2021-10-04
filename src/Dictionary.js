d00a23537d34258da8075bd7d460e62be796c12f
ASV185 <74805696+ASV185@users.noreply.github.com>
2021-10-03 23:25:30 -0600

    Added Photos API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,25 +2,37 @@ import React,{useState} from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
-import "./App";
 import Loader from "react-js-loader";
+import Photos from "./Photos";
+
 
 export default function Dictionary(props){
 
     let [keyword, setKeyword]=useState(props.defaultKeyword);
     let [results, setResults]=useState(null);
     let [loaded, setLoaded]=useState(false);
+    let [photos, setPhotos]=useState(null);
 
-    function handleResponse(response){
-        console.log(response.data[0]);
+    function handleDictionResponse(response){
         setResults(response.data[0]);
     }
 
+    function handlePexelsResponse(response){
+        setPhotos(response.data.photos);
+    }
+
     function search(){
+
      //documentation:https://dictionaryapi.dev/
         let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionResponse);
+
+        let pexelsApiKey="563492ad6f917000010000014927e3766700435cad1a0efc634e479c";
+        let pexelsApiUrl=`https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        let headers={Authorization:`Bearer ${pexelsApiKey}`};
+        axios.get(pexelsApiUrl, {headers:headers}).then(handlePexelsResponse);
     }
+
     function handleSubmit(event){
         event.preventDefault();
         search();
@@ -43,6 +55,7 @@ export default function Dictionary(props){
             </form>
         
             <Results results={results}/>
+            <Photos photos={photos}/>
         </div>
     );
 }else {
